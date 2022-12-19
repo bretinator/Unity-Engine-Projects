a5725956947d6143a82acc33108f037b8255483a
bretinator <118284145+bretinator@users.noreply.github.com>
2022-12-19 11:35:34 -0600

    Update DestroyAfterCustomTime.cs

--- SuperHeroGame/Scripts/DestroyAfterCustomTime.cs
+++ SuperHeroGame/Scripts/DestroyAfterCustomTime.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ * Destroys an object after a hardcoded amount of time.
+ * This program is only used to destroy the laser beams.
+ * Bret Shepard
+*/
+
 public class DestroyAfterCustomTime : MonoBehaviour
 {
     private void Start()
