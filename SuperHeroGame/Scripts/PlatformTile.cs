b6bb0daef2751d192f0ba6dbe1f1e9b10be620c8
bretinator <118284145+bretinator@users.noreply.github.com>
2022-12-19 12:01:17 -0600

    Update PlatformTile.cs

--- SuperHeroGame/Scripts/PlatformTile.cs
+++ SuperHeroGame/Scripts/PlatformTile.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ * Contains the startpoint and endpoint
+ * of the game tile so we know which
+ * end to place more tiles.
+ * Bret Shepard
+*/
+
 public class PlatformTile : MonoBehaviour
 {
     public Transform startPoint;
